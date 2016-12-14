dc6c09b87acaa01a53e0a31541ef4e6a215b49bc
neby56 <johnny101091@hotmail.com>
2016-12-14 17:02:10 +0100

    fin de la journée du 14/12/2016

--- app/Views/profil/parametre.php
+++ app/Views/profil/parametre.php
@@ -9,28 +9,37 @@ $this->start('main_content');
 <ul class="list-unstyled list-inline">
     <li><a href="<?= $this->url('profil_profil') ?>">Retour</a></li>
 </ul>
-<br/>
-<label>Pseudo</label><br/>
-Pseudo actuel<input type="text" class="form-control input-sm" value="<?= $w_user['usr_pseudo']?>"/>
-Nouveau pseudo <input type="text" name="" class="form-control input-sm" value="">
-<input class="pull-right btn" type="submit" value="Envoyer"/>
-<br/>
-<br/>
-<label>Votre email</label><br/>
-Adresse e-mail actuelle <input type="text" name="email" class="form-control input-sm" value="<?= $w_user['usr_email']?>">
-Nouvel e-mail <input type="text" name="" class="form-control input-sm" value="">
-<input class="pull-right btn" type="submit" value="Envoyer"/>
-<br/>
-<br/>
-<label>Changer votre mot de passe</label><br/>
-Ancien mot de passe <input type="password" class="form-control input-sm" name="ancienPassword">
-Nouveau mot de passe <input type="password" class="form-control input-sm"name="password">
-Réécrire nouveau mot de passe <input type="password" class="form-control input-sm" name="ConfirmPassword">
-<input type="submit" class="pull-right btn" value="Envoyer" />
-<br/>
-<br/>
-<label>Supprimer votre compte</label><br/>
-<a href="<?= $this->url('profil_delete') ?>">Je veux supprimer mon compte</a><br/>
+<div class="col-lg-3">
+<img class="img-circle" src="<?= $this->assetUrl('img/avatar/avatar_defaut.png'); ?>"/>
+</div>
+<div class="col-lg-9">
+    <form action="" method="post" enctype="multipart/form-data">
+        <label>Pseudo</label><br/>
+        Pseudo actuel<input type="text" class="form-control input-sm" name="pseudo" value="<?= $w_user['usr_pseudo'] ?>"/>
+        Modifier avatar<input type="file" name="file"/>
+        <input class="pull-right btn" type="submit" value="Modifier"/>
+    </form>
+    <br/>
+    <br/>
+    <form action="" method="post">
+        <label>Votre email</label><br/>
+        Adresse e-mail actuelle <input type="text" name="email" class="form-control input-sm" name="email" value="<?= $w_user['usr_email'] ?>">
+        <input class="pull-right btn" type="submit" value="Modifier"/>
+    </form>
+    <br/>
+    <br/>
+    <form action="" method="post">
+        <label>Changer votre mot de passe</label><br/>
+        Ancien mot de passe <input type="password" class="form-control input-sm" name="ancienPassword">
+        Nouveau mot de passe <input type="password" class="form-control input-sm"name="password">
+        Réécrire nouveau mot de passe <input type="password" class="form-control input-sm" name="ConfirmPassword">
+        <input type="submit" class="pull-right btn" value="Envoyer" />
+    </form>
+    <br/>
+    <br/>
+    <label>Supprimer votre compte</label><br/>
+    <a href="<?= $this->url('profil_delete') ?>">Je veux supprimer mon compte</a><br/>
+</div>
 <?php
 //fin du bloc
 $this->stop('main_content');
