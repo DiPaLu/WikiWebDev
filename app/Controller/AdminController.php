fec6b21d6cdccfc114c4b6aba0a53653e8412eb0
goidel <hovnerhands@gmail.com>
2016-12-16 14:58:46 +0100

    ajouts page admin: listes des utilisateurs affiche par role, avec  liens vers profils. AdminModel supprimé. Methode de requete ré-écrite dans UsersModel. Toutes les actions des méthodes Admin regroupes en une methode dans le Controlleur Admin.

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -3,8 +3,7 @@
 namespace Controller;
 
 use \W\Controller\Controller;
-use \Model\AdminModel;
-
+use \Model\UsersModel;
 
 /**
  * Description of admin
@@ -12,71 +11,18 @@ use \Model\AdminModel;
  * @author Etudiant
  */
 class AdminController extends Controller {
-    //put your code here
-       
+
+    //put your code   
     public function getAdmin() {
-          //$loggedUser = $this->getUser();
-          $adminModel = new AdminModel();
-          $usersList = $adminModel->getAllUsers();
-          $usersListUtilisateur = $adminModel->getUsersByRoleUtilisateur();
-          $usersListModerateur = $adminModel->getUsersByRoleModerateur();
-          $usersListAdmin = $adminModel->getUsersByRoleAdmin();
+        $loggedUser = $this->getUser();
+        $usersModel = new UsersModel();
+        $usersList = $usersModel->getAllUsers();
+           
 
-          $this->show('admin/admin', array(
-
-              'usersList' => $usersList,
-              'usersListUtilisateur' => $usersListUtilisateur,
-              'usersListModerateur' => $usersListModerateur,
-              'usersListAdmin' => $usersListAdmin
-          ));
+        $this->show('admin/admin', array(
+            'usersList' => $usersList,
+        ));
+        
     }
-    public function getTermsList() {
-          $adminModel = new AdminModel();
-          $termsList = $adminModel->getAllTerms();
 
-          $this->show('admin/admin', array(
-
-              'termsList' => $termsList
-          ));
-    }
-    public function getCategoriesList() {
-          $adminModel = new AdminModel();
-          $categoriesList = $adminModel->getAllCategories();
-
-          $this->show('admin/admin', array(
-
-              'categoriesList' => $categoriesList
-          ));
-    }
-    /*
-     * public function registerAdmin(){
-	//autorise l'accès à cette page seulement aux 'superadmin'
-	//le tableau n'est pas nécessaire lorsqu'il n'y a qu'un rôle à spécifier
-	$this->allowTo('admin');
-
-	//reste du code...
-    }
-    
-    public function viewAllUsers(){
-	//autorise l'accès à cette page aux utilisateurs ayant le rôle 'admin' ou 'superadmin'
-	//doit normalement être placé à la première ligne de chaque méthode à protéger
-	$this->allowTo(['admin']);
-
-	//reste du code...
-    }
-    public function editUserInfo(){
-	//autorise l'accès à cette page aux utilisateurs ayant le rôle 'admin' ou 'superadmin'
-	//doit normalement être placé à la première ligne de chaque méthode à protéger
-	$this->allowTo('admin');
-
-	//reste du code...
-    }
-    public function deleteUser(){
-	//autorise l'accès à cette page aux utilisateurs ayant le rôle 'admin' ou 'superadmin'
-	//doit normalement être placé à la première ligne de chaque méthode à protéger
-	$this->allowTo(['admin']);
-
-	//reste du code...
-    }
-    */
 }
