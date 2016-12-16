f97e893f7824647d198bacc682a176616bc77450
neby56 <johnny101091@hotmail.com>
2016-12-16 16:38:46 +0100

    fausse modif

--- app/Controller/ProfilController.php
+++ app/Controller/ProfilController.php
@@ -7,66 +7,75 @@ use \Model\UsersModel;
 use \W\Security\AuthentificationModel;
 
 class ProfilController extends Controller{
- 
-    
+
+
     public function parametre(){
         $this->show('profil/parametre');
     }
-    
+
     public function parametrePost(){
         $errorList = array();
         $successList = array();
-        
+
         $pseudo = isset($_POST['pseudo']) ? trim(strip_tags($_POST['pseudo'])) : '';
         $email = isset($_POST['email']) ? trim($_POST['email']) : '';
         $formOk = true;
-        
+
         if(empty($pseudo)){
             $errorList[] = 'Pseudo vide<br/>';
             $formOk = false;
         }
-        
-        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
-            $errorList[] = 'Email invalide<br>';
-            $formOk = false;
-        }
+
         if(empty($email)){
             $errorList[] = 'Email vide<br/>';
             $formOk = false;
         }
+        else if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+            $errorList[] = 'Email invalide<br>';
+            $formOk = false;
+        }
+
+        $usersModel = new UsersModel();
+
+        if($usersModel->emailExists($email)){
+            $errorList[] = 'L\'email existe déjà dans la base de données <br/>';
+            $formOk = false;
+        }
+
+        if($usersModel->getUserByUsernameOrEmail($pseudo)){
+            $errorList[] = 'Pseudo déjà utilisé <br/>';
+            $formOk = false;
+        }
+
+        $connected = $this->getUser();
+
+        if($formOk){
+            $auth = new AuthentificationModel();
+            $userData = $usersModel->update(array(
+                'usr_pseudo' => $pseudo,
+                'usr_email' => $email,
+            ), $connected['usr_id']);
+
+            if($userData !== false){
+                $successList = 'La modification a bien été faite!';
+                $auth->refreshUser();
+            }
+        }
+        
+        $this->show('profil/parametre', array(
+            'errorList' => $errorList,
+            'pseudo' => $pseudo,
+            'email' => $email,
+            'successList' => $successList
+        ));
     }
-    
+
     public function delete(){
         $this->show('profil/delete');
     }
-    
+
     public function profil(){
-        
         $data = $this->getUser();
-
-        
         $this->show('profil/profil');
-
-    }  
+    }
 }
-
-
-/*require 'inc/config.php';
-$infoUser = array();
-
-$idUser = isset($_POST['id']) ? intval($_POST['id']) : 0;
-
-$sql = 'SELECT * FROM users WHERE usr_id=:id';
-
-$pdoStatement = $pdo->prepare($sql);
-$pdoStatement->bindValue(':id', $idUser, PDO::PARAM_INT);
-
-if(!pdoStatement->execute()){
-	print_r($pdoStatement->errorInfo());
-}else{
-	$infoUser = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
-}
-
-
-
-*/
--- app/Views/profil/parametre.php
+++ app/Views/profil/parametre.php
@@ -6,6 +6,18 @@ $this->layout('layoutBootstrap', ['title' => 'Profil']);
 // Début du bloc d'affichage
 $this->start('main_content');
 ?>
+ <?php if (sizeof($errorList) > 0) : ?>
+            <?php foreach ($errorList as $currentError) : ?>
+                <div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <?= $currentError ?></div>
+            <?php endforeach; ?>
+        <?php endif; ?>
+        <?php if (sizeof($successList) > 0) : ?>
+            <?php foreach ($successList as $currentSuccess) : ?>
+                <div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <?= $currentSuccess ?></div>
+            <?php endforeach; ?>
+        <?php endif; ?>
 <ul class="list-unstyled list-inline pull-right">
     <li><a href="<?= $this->url('profil_profil') ?>">Afficher</a></li>
 </ul>
@@ -19,7 +31,6 @@ $this->start('main_content');
     <form action="" method="post" enctype="multipart/form-data">
         <label>Pseudo</label><br/>
         Pseudo actuel<input type="text" class="form-control input-sm" name="pseudo" value="<?= $w_user['usr_pseudo'] ?>"/>
-        <input class="pull-right btn" type="submit" value="Modifier"/>
         <br/>
     </form>
     <br/>
@@ -27,7 +38,6 @@ $this->start('main_content');
     <form action="" method="post">
         <label>Votre email</label><br/>
         Adresse e-mail actuelle <input type="text" name="email" class="form-control input-sm" name="email" value="<?= $w_user['usr_email'] ?>">
-        <input class="pull-right btn" type="submit" value="Modifier"/>
     </form>
     <br/>
     <br/>
@@ -36,7 +46,7 @@ $this->start('main_content');
         Ancien mot de passe <input type="password" class="form-control input-sm" name="ancienPassword">
         Nouveau mot de passe <input type="password" class="form-control input-sm"name="password">
         Réécrire nouveau mot de passe <input type="password" class="form-control input-sm" name="ConfirmPassword">
-        <input type="submit" class="pull-right btn" value="Envoyer" />
+        <input type="submit" class="pull-right btn" value="Modifier" />
     </form>
     <br/>
     <br/>
