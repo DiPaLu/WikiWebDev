7e9cee72d7ba26859779f3866a03a712290cefcd
neby56 <johnny101091@hotmail.com>
2016-12-22 17:59:44 +0100

    comit fin de journée

--- app/Controller/ProfilController.php
+++ app/Controller/ProfilController.php
@@ -37,6 +37,9 @@ public function parametrePost(){
     $usersModel = new UsersModel();
     $max = isset($_POST['MAX_FILE_SIZE']) ? $_POST['MAX_FILE_SIZE'] : '';
 
+    //debug($_POST);
+   // debug($passwordFormulaire);
+
     //pseudo
     if($pseudoFormulaire == 1){
         if(empty($pseudo)){
@@ -59,7 +62,6 @@ public function parametrePost(){
         }
     }
     //avatar
-    debug($max);
     if($max == 1){
         if($_FILES['file']['size']){
             $file = $_FILES['file'];
@@ -129,35 +131,35 @@ public function parametrePost(){
 
 
     //password
-    if(empty($ancienPassword)){
-        $errorList[] = 'Ancien password vide';
-        $formOk = false;
-        if(empty($password)){
-            $errorList[] = 'Password vide';
+    if($passwordFormulaire == 1){
+        if(empty($ancienPassword)){
+            $errorList[] = 'Ancien password vide';
             $formOk = false;
-            if(empty($confirmPassword)){
-                $errorList[] = 'Confirmation de password vide';
+            if(empty($password)){
+                $errorList[] = 'Password vide';
                 $formOk = false;
-            }
-            
-            if($password != $confirmPassword){
-                $errorList[] = 'Les passwords ne sont pas identiques';
-                $formOk = false;
+                if(empty($confirmPassword)){
+                    $errorList[] = 'Confirmation de password vide';
+                    $formOk = false;
+                }
+
+                if($password != $confirmPassword){
+                    $errorList[] = 'Les passwords ne sont pas identiques';
+                    $formOk = false;
+                }
             }
         }
+
+        if($formOk){
+            $data = $usersModel->update(array(
+                'usr_password' => $auth->hashPassword($password)
+            ), $id);
+
+            if($data !== FALSE){
+                $successList[] =  'Le mot de passe a été changé';
+            }
+        } 
     }
-    
-    if($formOk){
-        $data = $usersModel->update(array(
-            'usr_password' => $auth->hashPassword($password)
-        ), $id);
-        
-        if($data !== FALSE){
-            $successList[] =  'Le mot de passe a été changé';
-        }
-    }
-    
-    
     
 
     $this->show('profil/parametre', array(
@@ -169,71 +171,71 @@ public function parametrePost(){
     }
 
     public function delete(){
-    $this->show('profil/delete', array(
-    'errorList' => array(),
-     'successList' => array()
-    ));
+        $this->show('profil/delete', array(
+        'errorList' => array(),
+         'successList' => array()
+        ));
     }
 
     public function deletePost(){
-    $loggedUser = $this->getUser();
-    $email = $loggedUser['usr_email'];
-    $password = isset($_POST['password']) ? trim(strip_tags($_POST['password'])) : '';
-    $formOk = true;
+        $loggedUser = $this->getUser();
+        $email = $loggedUser['usr_email'];
+        $password = isset($_POST['password']) ? trim(strip_tags($_POST['password'])) : '';
+        $formOk = true;
 
-    if (empty($password)) {
-    $errorList[] = 'Password vide<br>';
-    $formOk = false;
-    }
+        if (empty($password)) {
+            $errorList[] = 'Password vide<br>';
+            $formOk = false;
+        } 
 
-    if($formOk){
-    $auth = new AuthentificationModel();
-    $userId = $auth->isValidLoginInfo($email, $password);
-    if($userId > 0){
-    $usersModel = new UsersModel();
-    $usersModel->delete($userId);
-    $sucessList[] = 'Votre compte a été supprimé';
-    $this->redirect('default_home');
-    }else{
-    $errorList[] = 'Mot de passe incorrect<br/>';
-    }
-    $sucessList[] = 'Compte supprimé avec succes';
-    $auth->logUserOut();
-    }
+        if($formOk){
+            $auth = new AuthentificationModel();
+            $userId = $auth->isValidLoginInfo($email, $password);
+        if($userId > 0){
+            $usersModel = new UsersModel();
+            $usersModel->delete($userId);
+            $sucessList[] = 'Votre compte a été supprimé';
+            $this->redirect('default_home');
+        }else{
+            $errorList[] = 'Mot de passe incorrect<br/>';
+        }
+            $sucessList[] = 'Compte supprimé avec succes';
+            $auth->logUserOut();
+        }
 
-    $this->show('lexique/terms');
+        $this->show('lexique/terms');
     }
 
 public function profil($pseudo){
-$profModel = new UsersModel();
-$termModel = new \Model\TermsModel();
-$prof = $profModel->getUserByUsernameOrEmail($pseudo);
-$connected = $this->getUser();
+    $profModel = new UsersModel();
+    $termModel = new \Model\TermsModel();
+    $prof = $profModel->getUserByUsernameOrEmail($pseudo);
+    $connected = $this->getUser();
 
-if($pseudo != '[:pseudo]'){
-$id = $connected['usr_id'];
-$pseudo = $prof['usr_pseudo'];
-$dateInscription = $prof['usr_insert_date'];
-$dateDerniereConnection = $prof['usr_last_connected'];
-$avatar = $prof['usr_avatar'];
-$pseudoUtilisateur = 'no';
-} else {
-$id = $connected['usr_id'];
-$pseudo = $connected['usr_pseudo'];
-$dateInscription = $connected['usr_insert_date'];
-$dateDerniereConnection = $connected['usr_last_connected'];
-$avatar = $connected['usr_avatar'];
-$pseudoUtilisateur = 'yes';
-}
-$nbMot = $termModel->getNombreMot($id);
+    if($pseudo != '[:pseudo]'){
+        $id = $connected['usr_id'];
+        $pseudo = $prof['usr_pseudo'];
+        $dateInscription = $prof['usr_insert_date'];
+        $dateDerniereConnection = $prof['usr_last_connected'];
+        $avatar = $prof['usr_avatar'];
+        $pseudoUtilisateur = 'no';
+    } else {
+        $id = $connected['usr_id'];
+        $pseudo = $connected['usr_pseudo'];
+        $dateInscription = $connected['usr_insert_date'];
+        $dateDerniereConnection = $connected['usr_last_connected'];
+        $avatar = $connected['usr_avatar'];
+        $pseudoUtilisateur = 'yes';
+    }
+    $nbMot = $termModel->getNombreMot($id);
 
-$this->show('profil/profil', array(
-'pseudo' => $pseudo,
- 'dateInscription' => $dateInscription,
- 'dateDerniereConnection' => $dateDerniereConnection,
- 'avatar' => $avatar,
- 'pseudoUtilisateur' => $pseudoUtilisateur,
- 'nbMot' => $nbMot
-));
-}
+    $this->show('profil/profil', array(
+        'pseudo' => $pseudo,
+         'dateInscription' => $dateInscription,
+         'dateDerniereConnection' => $dateDerniereConnection,
+         'avatar' => $avatar,
+         'pseudoUtilisateur' => $pseudoUtilisateur,
+         'nbMot' => $nbMot
+        ));
+    }
 }
